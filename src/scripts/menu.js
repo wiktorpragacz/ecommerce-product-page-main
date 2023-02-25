193a8586ddb77b24b77a02c6fb4a9f9e20180ade
wiktorp <110673073+wiktorpragacz@users.noreply.github.com>
2023-02-25 13:23:26 +0100

    Add files via upload

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,18 +1,23 @@
 import { cart } from "./cart.js";
 
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.hidden-menu');
-const links = document.querySelectorAll('a');
-const bodyBlur = document.querySelector('.show-menu-blur');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".hidden-menu");
+const bodyBlur = document.querySelector(".show-menu-blur");
+const menuIcon = menuBtn.querySelector("img");
 
-menuBtn.addEventListener('click', showMenu);
+menuBtn.addEventListener("click", showMenu);
 
 function showMenu() {
-    cart.classList.remove('show-cart')
-    window.scrollTo(0, 0)
-    menu.classList.toggle('show-menu');
-    document.body.classList.toggle('show-menu-body')
-    bodyBlur.classList.toggle('show')
+  setIcon();
+  cart.classList.remove("show-cart");
+  window.scrollTo(0, 0);
+  menu.classList.toggle("show-menu");
+  document.body.classList.toggle("show-menu-body");
+  bodyBlur.classList.toggle("show");
 }
 
-
+function setIcon() {
+  menu.classList.contains("show-menu")
+    ? menuIcon.setAttribute("src", "./images/icon-menu.svg")
+    : menuIcon.setAttribute("src", "./images/icon-close.svg");
+}

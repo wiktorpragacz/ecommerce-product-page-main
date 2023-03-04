901055eec4ced671af6a5f84e9e6cc32be1d353e
wiktorp <110673073+wiktorpragacz@users.noreply.github.com>
2023-03-04 23:08:54 +0100

    Add files via upload

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -21,27 +21,4 @@ window.addEventListener("click", (e) => {
     showCart();
   }
 });
-=======
-export { cart };
-export { cartProductContainer };
 
-const cartBtn = document.querySelector(".cart-btn");
-const cart = document.querySelector(".cart");
-const cartProductContainer = document.querySelector(".cart-products-container");
-
-function showCart() {
-  cart.classList.toggle("show-cart");
-}
-
-window.addEventListener("click", (e) => {
-  if (e.target.matches(".shopping-cart-icon")) {
-    showCart();
-  }
-  if (
-    cart.classList.contains("show-cart") &&
-    !e.target.matches(".shopping-cart-icon")
-  ) {
-    showCart();
-  }
-});
->>>>>>> 6bc287bf62889f865903eff8c73d09906535309b

8ff5c937bc45424de93cd7700a92d7f39e2ad3dc
wiktorp <110673073+wiktorpragacz@users.noreply.github.com>
2023-03-05 13:28:22 +0100

    Add files via upload

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,7 +1,6 @@
 export { cart };
 export { cartProductContainer };
 
-const cartBtn = document.querySelector(".cart-btn");
 const cart = document.querySelector(".cart");
 const cartProductContainer = document.querySelector(".cart-products-container");
 

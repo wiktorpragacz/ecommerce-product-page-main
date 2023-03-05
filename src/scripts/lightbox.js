6f15357910a04389e19514a986ad9aaa13c1b275
wiktorp <110673073+wiktorpragacz@users.noreply.github.com>
2023-03-05 13:27:45 +0100

    Add files via upload

--- src/scripts/lightbox.js
+++ src/scripts/lightbox.js
@@ -1,47 +1,34 @@
 import { formatThumbnailSrc } from "./slider.js";
 
 const thumbnails = document.querySelectorAll(".thumbnail-photo");
-const lightboxThumbnail = document.querySelectorAll(
-  ".lightbox-thumbnail-photo"
-);
 const lightboxThumbnailPhoto = document.querySelectorAll(
   ".lightbox-thumbnail-photo"
 );
 const lightbox = document.querySelector(".lightbox-container");
 const lightboxPhoto = document.querySelector(".lightbox-main-photo");
-const thumbailButtons = document.querySelectorAll(".thumbnail-btn");
+const thumbnailButtons = document.querySelectorAll(".thumbnail-btn");
 
 let photoIndex = 0;
 
-thumbailButtons.forEach((btn) => {
+thumbnailButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    deleteActiveClass();
-
     if (btn.matches(".next-btn-thumbnail")) {
-      deleteActiveClass();
-
       photoIndex++;
 
       if (photoIndex > thumbnails.length - 1) {
-        lightboxThumbnailPhoto[0].classList.add("active-thumbnail");
         photoIndex = 0;
       }
-
-      lightboxThumbnailPhoto[photoIndex].classList.add("active-thumbnail");
 
       const currentPhoto = formatThumbnailSrc(thumbnails[photoIndex]);
       lightboxPhoto.setAttribute("src", currentPhoto);
     }
 
     if (btn.matches(".prev-btn-thumbnail")) {
-      deleteActiveClass();
       photoIndex--;
 
       if (photoIndex < 0) {
-        lightboxThumbnailPhoto[3].classList.add("active-thumbnail");
-        photoIndex = 3;
+        photoIndex = thumbnails.length - 1;
       }
-      lightboxThumbnailPhoto[photoIndex].classList.add("active-thumbnail");
 
       const currentPhoto = formatThumbnailSrc(thumbnails[photoIndex]);
       lightboxPhoto.setAttribute("src", currentPhoto);
@@ -49,17 +36,15 @@ thumbailButtons.forEach((btn) => {
   });
 });
 
-function deleteActiveClass() {
-  lightboxThumbnailPhoto.forEach((photo) =>
-    photo.classList.remove("active-thumbnail")
-  );
-}
-
 window.addEventListener("click", (e) => {
   if (e.target.matches(".thumbnail-photo")) {
     const currentThumbnail = e.target;
-    // checks what thumbnail got clicked and set it as a main photo
+    // remove -thumbnail from photo src and set as current photo
     const currentPhoto = formatThumbnailSrc(currentThumbnail);
+
+    photoIndex = filterNumberFromSrc(currentPhoto) - 1;
+
+    // checks what thumbnail got clicked and set it as a main photo
     lightboxPhoto.setAttribute("src", currentPhoto);
 
     document.body.classList.add("hide-overflow");
@@ -83,13 +68,10 @@ function closeLightbox() {
 
 lightboxThumbnailPhoto.forEach((thumbnail) => {
   thumbnail.addEventListener("click", (e) => {
-    deleteActiveClass();
-    e.target.classList.add("active-thumbnail");
     const correctPhoto = formatThumbnailSrc(e.target);
 
     // find number in src and set it as photoIndex
     photoIndex = filterNumberFromSrc(correctPhoto);
-    lightboxPhoto.setAttribute("src", correctPhoto);
   });
 });
 
@@ -99,4 +81,3 @@ function filterNumberFromSrc(src) {
     .filter((element) => /[0-9]/.test(element))
     .join("");
 }
-
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,9 +1,7 @@
 export { formatThumbnailSrc };
 
-//       mobile photo slider
 const image = document.querySelector(".photos-section > img");
 const slideBtn = document.querySelectorAll(".slide-btn");
-const thumbnailPhoto = document.querySelectorAll(".thumbnail-photo");
 
 slideBtn.forEach((btn) => {
   btn.addEventListener(
@@ -19,10 +17,8 @@ function showPrevPhoto() {
   if (photoID < 1) {
     photoID = 4;
     image.setAttribute("src", `./images/image-product-4.jpg`);
-    removeActiveClass();
   } else {
     image.setAttribute("src", `./images/image-product-${photoID}.jpg`);
-    removeActiveClass();
   }
 }
 
@@ -32,38 +28,9 @@ function showNextPhoto() {
   if (photoID > 4) {
     photoID = 1;
     image.setAttribute("src", `./images/image-product-1.jpg`);
-    removeActiveClass();
   } else {
     image.setAttribute("src", `./images/image-product-${photoID}.jpg`);
-    removeActiveClass();
   }
-}
-//      desktop photo slider
-// default active state
-thumbnailPhoto[0].classList.add("active-photo");
-
-thumbnailPhoto.forEach((photo) => {
-  photo.addEventListener("click", setMainPhoto);
-});
-
-function setMainPhoto(e) {
-  // remove active class and set it on thumbnail photo
-  removeActiveClass();
-  Array.from(thumbnailPhoto).map((photo) =>
-    photo.classList.remove("active-photo")
-  );
-  const currentThumbnail = e.target;
-  currentThumbnail.classList.add("active-photo");
-  // remove'-thumbnail' from thumbnail photo url
-  const correctSrc = formatThumbnailSrc(currentThumbnail)
-  image.setAttribute("src", correctSrc);
-}
-
-function removeActiveClass() {
-  Array.from(thumbnailPhoto).map((photo) =>
-    photo.classList.remove("active-photo")
-  );
-  thumbnailPhoto[photoID - 1].classList.add("active-photo");
 }
 
 function formatThumbnailSrc(thumbnail) {
